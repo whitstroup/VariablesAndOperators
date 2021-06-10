db172867571affeccc00b0463fa6efedb4604719
Whit Stroup <email@example.com>
2021-06-10 12:16:38 -0500

    created

---
+++ VariablesAndOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariablesAndOperators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //TODO Create a program that asks for a number
+            Console.WriteLine("Hello and welcome!");
+
+            //TODO Take that number as an input and using a ternary operator respond with if that number is even or odd.
+
+            //TODO Assign the value of the double below to an int variable called "priceRounded". google "w3schools C# type casting. Try both explicit type casting and implicit type casting.
+
+            double price = 15.99;
+
+        }
+    }
+}
