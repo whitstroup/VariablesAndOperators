4335a1bdda4e6dbb6541bfb33262e34458589200
Whit Stroup <49410964+whitstroup@users.noreply.github.com>
2021-06-10 12:18:28 -0500

    Update Program.cs

--- VariablesAndOperators/Program.cs
+++ VariablesAndOperators/Program.cs
@@ -11,7 +11,7 @@ namespace VariablesAndOperators
 
             //TODO Take that number as an input and using a ternary operator respond with if that number is even or odd.
 
-            //TODO Assign the value of the double below to an int variable called "priceRounded". google "w3schools C# type casting. Try both explicit type casting and implicit type casting.
+            //TODO Assign the value of the double below to an int variable called "priceRounded". google "w3schools C# type casting"
 
             double price = 15.99;
 
